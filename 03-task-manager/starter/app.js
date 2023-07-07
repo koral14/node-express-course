84ad284818705bde1abce8c2fb877e2c4f678cd3
koral14 <105457134+koral14@users.noreply.github.com>
2023-07-07 13:59:23 -0700

    week6 - Task manager API part 2

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -3,16 +3,17 @@ const app = express()
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
+const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 
 // middleware
 app.use(express.json())
+app.use(express.static('./public'))
 
 // routes
-app.get('/hello', (req, res) => {
-    res.send('Task Manager App')
-})
-
 app.use('/api/v1/tasks', tasks)
+app.use(notFound)
+app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 3000
 
@@ -26,6 +27,8 @@ const start = async () => {
 }
 
 start()
+
+
 
 // app.get('api/v1/tasks') - get all the tasks
 // app.post('api/v1/tasks') - create a new task
