0ffe8b896dcc864e1f0e8e098343c3ee93044a39
koral14 <105457134+koral14@users.noreply.github.com>
2023-07-17 02:20:35 -0700

    week7 - Using Query Parameters

---
+++ 04-store-api/starter/controllers/products.js
@@ -0,0 +1,46 @@
+const Product = require('../models/product')
+
+const getAllProductsStatic = async (req, res) => {
+    const search = 'ab'
+    const products = await Product.find({}).sort('-name price')
+    res.status(200).json({ products, nbHits: products.length })
+}
+
+const getAllProducts = async (req, res) => {
+    const { featured, company, name, sort } = req.query
+    const queryObject = {}
+
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false
+    }
+
+    if (company) {
+        queryObject.company = company
+    }
+
+    if (name) {
+        queryObject.name = { $regex: name, $options: 'i' }
+    }
+
+    // console.log(queryObject);
+    let result = Product.find(queryObject)
+    
+    if (sort) {
+        // console.log(sort);
+        // products = products.sort()
+        const sortList = sort.split(',').join(' ')
+        result = result.sort(sortList)
+    } else {
+        result = result.sort('createdAt')
+    }
+
+    const products = await result
+
+    res.status(200).json({ products, nbHits: products.length })
+
+}
+
+module.exports = {
+    getAllProducts,
+    getAllProductsStatic,
+}
